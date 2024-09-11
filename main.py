d24fbe8a2d4bf3643d6427fbb82ad40ec37043b3
srushti1303 <srushtikamble1303@gmail.com>
2024-09-11 16:57:07 -0400

    data-ingestion

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.FaceMask import logger 
+from src.FaceMask.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/FaceMask/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/Users/srushtikamble/Documents/Projects/Face-Mask-Detection/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/srushtikamble/Documents/Projects/Face-Mask-Detection/params.yaml")
