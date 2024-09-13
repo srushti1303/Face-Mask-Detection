6ca6a7e319f08fab3b33d0eca32eb5589a56fc40
srushti1303 <srushtikamble1303@gmail.com>
2024-09-13 13:42:27 -0400

    dvc added

--- src/FaceMask/config/configuration.py
+++ src/FaceMask/config/configuration.py
@@ -2,7 +2,7 @@ import os
 from FaceMask.constants import *
 from FaceMask.utils.comman import read_yaml, create_directories
 from FaceMask.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,
-                                            PrepareCallbacksConfig, TrainingConfig)
+                                            PrepareCallbacksConfig, TrainingConfig, EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -90,3 +90,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.keras"),
+            training_data=Path("artifacts/data_ingestion/Dataset"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
