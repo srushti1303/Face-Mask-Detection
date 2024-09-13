59a5957fa50e385df2311edb0d0ff26ae764a4f1
srushti1303 <srushtikamble1303@gmail.com>
2024-09-13 12:33:53 -0400

    training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.FaceMask import logger 
 from src.FaceMask.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.FaceMask.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.FaceMask.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
